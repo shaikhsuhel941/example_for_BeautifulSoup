b60637f44d4db706aae0f149145f64bcae383db2
shaikhsuhel941 <shaikhsuhel941@gmail.com>
2022-01-27 14:49:47 +0530

    Initial commit

--- github_img.py
+++ github_img.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-# <img style="height:auto;" alt="" width="260" height="260" class="avatar avatar-user width-full border color-bg-default" src="https://avatars.githubusercontent.com/u/97778625?v=4">
-
 user_name = input('Enter your github user_name : ')
 url = f"https://github.com/{user_name}"
 source_code = requests.get(url).text
